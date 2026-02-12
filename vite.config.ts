34755b08f79dfc75be68a799277c9a7e227505a9
Milad Bahari Qaragoz <125036757+MiladBahariQaragoz@users.noreply.github.com>
2026-02-12 17:32:58 +0000

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import glsl from 'vite-plugin-glsl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfoliomultiverse/',
   plugins: [react(), glsl()],
   server: {
     host: true,
